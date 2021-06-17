f81c768473817bc992fa8eccee07daefc34ec255
adumbidiot <nathaniel.daniel23@outlook.com>
2021-06-17 16:47:53 -0700

    Seperate logger to module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod adapters_info;
 mod com_thread;
 mod console;
+mod logger;
 mod mac_spoof;
 mod settings;
 mod style;
@@ -27,83 +28,6 @@ use iced::{
     Settings,
 };
 use iced_aw::TabLabel;
-use once_cell::sync::Lazy;
-
-pub struct ThreadLogger {
-    sender: crossbeam_channel::Sender<(log::Level, String)>,
-
-    handle: std::thread::JoinHandle<()>,
-}
-
-impl ThreadLogger {
-    pub fn new() -> Self {
-        fn process_message((level, msg): (log::Level, &str)) {
-            match level {
-                log::Level::Info => {
-                    print!("\x1B[96m");
-                }
-                log::Level::Error => {
-                    print!("\x1B[91m");
-                }
-                log::Level::Warn => {
-                    print!("\x1B[93m");
-                }
-                _ => {}
-            }
-            print!("[{}] ", level);
-            print!("\x1B[0m");
-
-            println!("{}", msg);
-        }
-
-        let (tx, rx) = crossbeam_channel::unbounded::<(log::Level, String)>();
-        let handle = std::thread::spawn(move || {
-            process_message((log::Level::Info, "Starting logger thread"));
-
-            for (level, msg) in rx {
-                process_message((level, msg.as_str()))
-            }
-
-            process_message((log::Level::Info, "Shutting down logger thread"));
-        });
-
-        Self { sender: tx, handle }
-    }
-
-    pub fn close(self) -> anyhow::Result<()> {
-        drop(self.sender);
-        self.handle
-            .join()
-            .map_err(|_e| anyhow::anyhow!("logger thread panicked"))?;
-        Ok(())
-    }
-}
-
-impl log::Log for ThreadLogger {
-    fn enabled(&self, metadata: &log::Metadata) -> bool {
-        metadata.target().starts_with("hekk")
-    }
-
-    fn log(&self, record: &log::Record) {
-        if !self.enabled(record.metadata()) {
-            return;
-        }
-
-        self.sender
-            .send((record.level(), format!("{}", record.args())))
-            .expect("failed to send message to logger thread");
-    }
-
-    fn flush(&self) {}
-}
-
-impl Default for ThreadLogger {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-static LOGGER: Lazy<ThreadLogger> = Lazy::new(ThreadLogger::new);
 
 #[derive(Debug, Clone)]
 pub enum Message {
@@ -195,10 +119,7 @@ impl Application for App {
 }
 
 fn main() -> anyhow::Result<()> {
-    if let Err(e) = log::set_logger(&*LOGGER) {
-        anyhow::bail!("failed to set logger: {}", e);
-    }
-    log::set_max_level(log::LevelFilter::Info);
+    crate::logger::setup().context("failed to setup logger")?;
 
     let mut settings = Settings::default();
     settings.window.size = (640, 480);
